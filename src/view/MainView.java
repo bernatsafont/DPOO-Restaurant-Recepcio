9015ada3a6a3450d41e4ff1d2b19a1d176017722
Enrique <ls31202@salleurl.edu>
2018-04-05 18:59:29 +0200

    finestra definida v0.3

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -20,22 +20,12 @@ public class MainView extends JFrame{
 
         // create the utility panels
         JPanel jpTotal = new JPanel();
-        //JPanel jpDemanar = new JPanel();
-        //JPanel jpReservar = new JPanel();
 
-        // set layouts
-        //jpTotal.setLayout(new GridLayout(1,2));
-        //jpDemanar.setLayout(new GridBagLayout());
-        //jpReservar.setLayout(new GridBagLayout());
+
         jpTotal.setLayout(new GridBagLayout());
 
-        // set components into demanar window
-        //jpDemanar.setBorder(BorderFactory.createTitledBorder("Demanar taula"));
-        // set components into reservar window
-        //jpReservar.setBorder(BorderFactory.createTitledBorder("Reservar taula"));
-
         // set main to everything
-        jpTotal.setBorder(BorderFactory.createTitledBorder("agafar taula"));
+        jpTotal.setBorder(BorderFactory.createTitledBorder("Agafar taula"));
 
         // set components to the bag
         GridBagConstraints c = new GridBagConstraints();
@@ -75,11 +65,31 @@ public class MainView extends JFrame{
         c.gridwidth = 2;
         jpTotal.add(spinner,c);
 
-        // label with the number of comensals
-        JLabel jlReservation = new JLabel("Reservation Date:    ");
+        // ask or order
+        JCheckBox jcbAsk = new JCheckBox("Demanar");
+        jcbAsk.setSelected(true);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 2;
+        c.insets = new Insets(10,20,10,0);
+        c.gridwidth = 1;
+        jpTotal.add(jcbAsk, c);
+
+        JCheckBox jcbOrder = new JCheckBox("Reservar");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 2;
+        c.insets = new Insets(10,20,10,0);
+        c.gridwidth = 1;
+        jpTotal.add(jcbOrder, c);
+
+
+
+        // label with the number of reservation date
+        JLabel jlReservation = new JLabel("Reservation Date:    ");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 3;
         c.insets = new Insets(10,20,10,0);
         c.gridwidth = 1;
         jpTotal.add(jlReservation, c);
@@ -91,20 +101,17 @@ public class MainView extends JFrame{
         c.weightx = 0.5;
         c.insets = new Insets(10,0,10,20);
         c.gridwidth = 2;
+        spinnerDate.setEnabled(false);
         jpTotal.add(spinnerDate,c);
 
         // reservation button
         jbReservation = new JButton("Reservation");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 4;
         c.insets = new Insets(10,20,10,0);
-        c.gridwidth = 1;
+        c.gridwidth = 2;
         jpTotal.add(jbReservation, c);
-
-        // place panels to the main panel
-        //jpTotal.add(jpDemanar);
-        //jpTotal.add(jpReservar);
 
         getContentPane().add(jpTotal);
 
