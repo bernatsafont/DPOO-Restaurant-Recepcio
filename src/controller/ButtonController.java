43853fc7a333d95e8d5a93b85ba6ba1cf117feb8
spunkey20 <ls31202@salleurl.edu>
2018-04-07 00:43:38 +0200

    v1.5 - lasts connectivity with server and ok message

--- src/controller/ButtonController.java
+++ src/controller/ButtonController.java
@@ -1,22 +1,39 @@
+// package where it belongs
 package controller;
 
+// import local classes
 import view.MainView;
 
+// import java classes
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/***
+ * Class to control all the button actions
+ */
 public class ButtonController implements ActionListener{
 
+    // create a instance of the view
     private MainView mView;
 
+    /***
+     * Createor that sets the view and the network
+     * @param mView MainView instance to get the view
+     */
     public ButtonController(MainView mView) {
         this.mView = mView;
     }
 
+    /***
+     * Method that manages actions performed by the view it's listening
+     * @param e Event that is triggered
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        // look which button is pressed
         switch(e.getActionCommand()){
+            // if ask, select ask and disable the date table and actualize it
             case "Ask":
                 if(!mView.getStateAsk()){
                     mView.setSelectedAsk(true);
@@ -25,6 +42,8 @@ public class ButtonController implements ActionListener{
                 mView.setEnableDate(false);
                 mView.resetCounter();
                 break;
+
+            // if order, select the order checkbox, enable the date item and actualize the date
             case "Order":
                 if(!mView.getStateOrder()){
                     mView.setSelectedOrder(true);
@@ -33,6 +52,8 @@ public class ButtonController implements ActionListener{
                 mView.setEnableDate(true);
                 mView.resetCounter();
                 break;
+
+            // if reservation, check if all filled and send to the server
             case "Reservation":
                 String s = checkEntry();
                 if(!s.equals("OK")){
@@ -48,6 +69,10 @@ public class ButtonController implements ActionListener{
         }
     }
 
+    /***
+     * Method that looks if all the items are filled
+     * @return String with the result
+     */
     private String checkEntry(){
         StringBuilder sb = new StringBuilder();
         if(mView.getReservationName().equals("")){
--- src/model/ConnectivityData.java
+++ src/model/ConnectivityData.java
@@ -1,37 +1,53 @@
+// package where it belongs
 package model;
 
+// import java classes
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+// import extra java classes
 import org.json.*;
 
-
+/***
+ * Class that manages the model information related with the connectivity
+ */
 public class ConnectivityData {
 
+    // generate constant values
     private String IP;
     private int PORT;
     private final String PATH = "../DPOO-Restaurant-Servidor/resources";
 
+    /***
+     * Constructor of the class, sets the constants value
+     */
     public ConnectivityData() {
         JSONObject obj = new JSONObject(getFileContent("config.json"));
         this.IP = obj.getString("db_ip");
         this.PORT = Integer.parseInt(obj.getString("port_server"));
     }
 
-
+    /***
+     * Getter of IP fied
+     * @return String with the ip name
+     */
     public String getIP() {
         return IP;
     }
 
+    /***
+     * Getter of PORT field
+     * @return integer with the port number
+     */
     public int getPORT() {
         return PORT;
     }
 
-    /**
-     * MÃ©tode que a partir d'un nom d'arxiu retorna un string en format Json
-     * @param file nom del arxiu que es vol convertir en format .json
-     * @return String convertit en format .json
+    /***
+     * Method that from a file name returns a String in JSON format
+     * @param file name of the file that we want to convert to .json
+     * @return String converted in .json format
      */
     private String getFileContent(String file){
         //creem un scanner per al arxiu
