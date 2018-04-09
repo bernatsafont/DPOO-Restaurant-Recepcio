7c0376b4d100bd2cb4d3fd9c80ef677a2df9fe5c
oriolpando <oriolpando@gmail.com>
2018-04-09 23:25:35 +0200

    Resposta server feta

--- src/controller/ButtonController.java
+++ src/controller/ButtonController.java
@@ -83,11 +83,17 @@ public class ButtonController implements ActionListener{
 
                     // wait server answer
                     try{
+
                         boolean answer = rNetwork.getAnswer();
                         String code = rNetwork.getCode();
+                        System.out.println(answer);
+                        System.out.println(code);
                         if(answer){
                             mView.popWindow(mView, "Your table code is: " + code, "Now you have a table", "Info");
                             //TODO: BUIDAR ELS CAMPS DE LA VISTA UN COP LA RESERVA ESTA FETA
+                        }else{
+                            mView.popWindow(mView, code, "Unable to reserve a table", "Error");
+
                         }
                     }catch (Exception e1){
                         mView.popWindow(mView, e1.getMessage(), "Server Error", "Error");
