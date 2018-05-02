fc384e3066251deff23f4de74ee570befbf3fac6
spunkey20 <ls31202@salleurl.edu>
2018-05-02 23:50:50 +0200

    tot comentat

--- src/network/MailNetwork.java
+++ src/network/MailNetwork.java
@@ -1,5 +1,6 @@
+// 
 package network;
-
+// 
 
 import java.util.Properties;
 import java.util.*;
@@ -42,26 +43,27 @@ public class MailNetwork extends Thread{
 
         // generate and set mail propierties
         Properties properties = System.getProperties();
-
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "587");
         properties.put("mail.smtp.auth", "true");
         properties.put("mail.smtp.starttls.required", "true");
 
-
-
+        // set send data
         java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         Session mailSession = Session.getDefaultInstance(properties, null);
         mailSession.setDebug(false);
         Message msg = new MimeMessage(mailSession);
         msg.setFrom(new InternetAddress(origin));
         InternetAddress[] address = {new InternetAddress(to)};
+
+        // write mail
         msg.setRecipients(Message.RecipientType.TO, address);
         msg.setSubject("Table Reservation");
         msg.setSentDate(new Date());
         msg.setText(message);
 
+        // send message
         Transport transport=mailSession.getTransport("smtp");
         transport.connect(host, origin, password);
         transport.sendMessage(msg, msg.getAllRecipients());
